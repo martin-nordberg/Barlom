e5a8d2621def18b4e669f39234d1351ee7c65846
Martin Nordberg <martinnordberg3@gmail.com>
2021-03-09 08:29:09 -0500

    WIP: Experiments with Docker and Kubernetes

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,4 +1,5 @@
 
-call mvn clean package
+mvn clean package
 docker build -t microk8s.mshome.net:32000/barlom .
+docker push microk8s.mshome.net:32000/barlom
 docker run -it --rm -p:8080:8080 -p:8081:8081 microk8s.mshome.net:32000/barlom
--- run-in-k8s.sh
+++ run-in-k8s.sh
@@ -1,10 +1,10 @@
 
-call mvn clean package
+mvn clean package
 docker build -t microk8s.mshome.net:32000/barlom .
 
 docker push microk8s.mshome.net:32000/barlom
 
-kubectl delete -n default replicaset barlom
-kubectl delete -n default service barlom
+kubectl delete -n default deployment barlom-app
+kubectl delete -n default service barlom-app
 
 kubectl apply -f barlom-service.yaml
